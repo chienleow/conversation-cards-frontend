cc048fd973d4c3257bcef86d1b23bca3c197a637
chienleow <chienleow1@gmail.com>
2021-01-17 15:49:50 -0500

    remove unneccesary js files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 let reducer = ''
 
@@ -23,4 +22,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
