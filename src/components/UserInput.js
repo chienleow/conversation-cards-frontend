00c7263d00eec5756118559a50d8da2a4bd11ade
chienleow <chienleow1@gmail.com>
2021-01-25 14:21:11 -0500

    handleChange

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -7,7 +7,9 @@ class UserInput extends React.Component {
     state = {name: ''}
 
     handleChange = (event) => {
-        debugger
+        this.setState({
+            name: event.target.value
+        })
     }
 
     // need state to create a form, onchange, handlesubmit
@@ -18,7 +20,7 @@ class UserInput extends React.Component {
                 <Form>
                     <Form.Group controlId="formBasicEmail">
                         {/* <Form.Label>Username</Form.Label> */}
-                        <Form.Control type="text" placeholder="Enter username" value={this.state.name} onChange={this.handleChange}/>
+                        <Form.Control type="text" placeholder="Enter username" value={this.state.name} name="name" onChange={this.handleChange}/>
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
