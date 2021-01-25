a45d76a17575cefd24012030d89582cb21ef32dc
chienleow <chienleow1@gmail.com>
2021-01-25 15:42:48 -0500

    fetch post to users

--- src/actions/addUser.js
+++ src/actions/addUser.js
@@ -1,6 +1,14 @@
 export const addUser = (data) => {
 
     return (dispatch) => {
-        
+        fetch('http://localhost:3040/api/v1/users', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+
     }
 }
