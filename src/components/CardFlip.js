3326e94222d12be5c6824dba894e250c3e650858
chienleow <chienleow1@gmail.com>
2021-01-28 21:22:47 -0500

    add CardFlip to map

--- src/components/CardFlip.js
+++ src/components/CardFlip.js
@@ -10,30 +10,40 @@ const CardFlip = (props) => {
     setIsFlipped(!isFlipped);
   };
 
-      return (
-        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Text>
-                {!!props.questions ? props.questions.map(question => <li key={question.id}>{question.attributes.sentence} - {question.attributes.user.name} - {question.attributes.category}</li> ):'loading'}
-              </Card.Text>
-              <Button onClick={handleClick}>Open Question</Button>
-            </Card.Body>
-          </Card>
-    
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <Button onClick={handleClick}>Back</Button>
-            </Card.Body>
-          </Card>
-        </ReactCardFlip>
-      )
+      render () {
+        if (!!props.questions) {
+          return (
+            <div>
+            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Text>
+                    {!!props.questions ? props.questions.map(question => <li key={question.id}>{question.attributes.sentence} - {question.attributes.user.name} - {question.attributes.category}</li> ):'loading'}
+                  </Card.Text>
+                  <Button onClick={handleClick}>Open Question</Button>
+                </Card.Body>
+              </Card>
+        
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                  </Card.Text>
+                  <Button onClick={handleClick}>Back</Button>
+                </Card.Body>
+              </Card>
+            </ReactCardFlip>
+            </div>
+          ) else {
+            return (
+              <div>loading</div>
+            )
+          };
+        };
+      };
 
-}
+};
 
-  export default CardFlip;
+export default CardFlip;
